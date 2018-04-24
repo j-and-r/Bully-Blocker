76c35cc79087f98ce92ea0c4e329ca44b6733fab
Rohan van Klinken <rohanarievk@gmail.com>
2018-04-24 16:58:04 +1000

    Made basic mean word finder
    It is very basic!

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
 
+tweet = "i hate you"
+words = ["hate", "idiot", "stupid"]
+wordcount = 0
+
 app = Flask(__name__)
+
+def meanDetector():
+    meancount = 0
+    wordcount = len(tweet.split(" "))
+    for word in words:
+        if word in tweet:
+            meancount += 1
+    ratio = meancount/wordcount*100
+    return ratio
+
+print(str(meanDetector()) + "%")
 
 @app.route("/")
 def index():
