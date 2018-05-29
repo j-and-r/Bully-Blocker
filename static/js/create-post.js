39729aeb42aef5ac792c66595211cfec51e6ed46
Rohan van Klinken <rohanarievk@gmail.com>
2018-05-29 17:15:00 +1000

    changed request header

--- static/js/create-post.js
+++ static/js/create-post.js
@@ -10,7 +10,7 @@ window.onload = function() {
         'content-type': 'application/json'
       },
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, cors, *same-origin
+      mode: 'no-cors', // no-cors, cors, *same-origin
       redirect: 'follow', // manual, *follow, error
       referrer: 'no-referrer', // *client, no-referrer
     })
